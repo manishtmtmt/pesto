3f969f6ec1c80c0d17242a03dec3ed5a12e1bcf9
Manish Tiwari <manisht.mt.mt@gmail.com>
2023-06-15 08:18:53 +0530

    session-12 async await solution

---
+++ Session-12_Async_JS/Assignments/AsyncAwait/script.js
@@ -0,0 +1,8 @@
+// https://api.exchangerate.host/latest
+
+async function getExchangeRate(currencyCode) {
+  const response = await fetch(`https://api.exchangerate.host/latest`);
+  const data = await response.json();
+  const rates = data.rates;
+  return rates[currencyCode] ? rates[currencyCode].toFixed(4) : null;
+}
